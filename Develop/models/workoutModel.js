dd9ac56d83b8675bcf6f095998d31834f3b22ef1
Aurora Brune <akbrune@hotmail.com>
2020-10-04 15:48:23 -0500

    aded api-routes file, name change workoutModel.js

---
+++ Develop/models/workoutModel.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({    
+    workout: [
+        {
+            lastActiveAt: Date(),
+            exercises: [
+                {
+                    type: {
+                        type: String,
+                        trim: true,
+                        required: "Type is Required"
+                    },
+
+                    name: {
+                        type: String,
+                        trim: true,
+                        required: "Name is Required"
+                    },
+
+                    duration: {
+                        type: Number,
+                    },
+
+                    weight: {
+                        type: Number
+                    },
+
+                    reps: {
+                        type: Number
+                    },
+
+                    sets: {
+                        type: Number
+                    }
+                }
+            ]
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
+
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const WorkoutSchema = new Schema({
+//     workout: [
+//         {
+//             lastActiveAt: Date(),
+//             exercises: [
+//                 {
+//                     type: String,
+//                     name: String,
+//                     duration: Number,
+//                     weight: Number,
+//                     reps: Number,
+//                     sets: Number,
+//                 }
+//             ]
+//         }
+//     ]
+// });
+
+// const Workout = mongoose.model("Workout", WorkoutSchema);
+
+// module.exports = Workout;
